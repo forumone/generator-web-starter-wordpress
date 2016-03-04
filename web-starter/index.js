2cb067b73f3f2e8e8184a5692c1463de4258fd4e
William Hurley <whurley@forumone.com>
2016-03-04 15:58:42 -0500

    Using additional theme_path attribute. Related to forumone/generator-web-starter#35

--- web-starter/index.js
+++ web-starter/index.js
@@ -46,7 +46,9 @@ module.exports = generators.Base.extend({
       }
     },
     setThemePath : function() {
-      this.options.parent.answers.theme_gemfile = 'public/wp-content/themes/' + this.options.parent.answers['web-starter-wordpress'].wordpress_theme + '/Gemfile';
+      var theme_path = 'public/wp-content/themes/' + this.options.parent.answers['web-starter-wordpress'].wordpress_theme;
+      this.options.parent.answers.theme_path = theme_path;
+      this.options.parent.answers.theme_gemfile = theme_path + '/Gemfile';
     }
   },
   writing : {
