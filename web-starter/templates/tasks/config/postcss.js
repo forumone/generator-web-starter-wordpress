ccd550291370ef66f1d9fd067bfd469e78f75ad0
Thibaut Lassalle <thibaut.lassalle@gmail.com>
2016-04-15 16:58:36 -0400

    add grunt task for generator-web-starter-grunt

---
+++ web-starter/templates/tasks/config/postcss.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+  var assets  = require('postcss-assets');
+
+  grunt.config.merge({
+    postcss : {
+      options : {
+        // map: {
+        //   inline: false // save all sourcemaps as separate files...
+        // },
+        processors: [
+          require('postcss-assets')(),
+          require('autoprefixer')({
+            browsers: '> 1%, last 3 versions',
+            remove: false // don't remove outdated prefixes (there shouldn't be any, and this saves compile time)
+          }),
+        ]
+      },
+      theme: {
+        src: 'build/css/*.css'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-postcss');
+}
+
